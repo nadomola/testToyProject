d8433c5cb5d73de58012eae2960b0996fd2095e2
nadomola <[siyun778@gamil.com]>
2024-01-15 23:59:52 +0900

    tnwjd

--- demo/src/main/java/com/example/demo/dto/ResponseDTO.java
+++ demo/src/main/java/com/example/demo/dto/ResponseDTO.java
@@ -13,5 +13,6 @@ import java.util.List;
 @AllArgsConstructor
 @Data
 public class ResponseDTO<T> {
-
+    private String error; //에러 객체 생성
+    private List<T> data;  // data의 묶음 - DB에서 조회한 데이터 묶음을 리스트에 담긴 T타입으로 가져옴
 }
--- demo/src/main/java/com/example/demo/model/UserEntity.java
+++ demo/src/main/java/com/example/demo/model/UserEntity.java
@@ -9,17 +9,23 @@ import org.hibernate.annotations.GenericGenerator;
 import javax.persistence.*;
 
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor //기본 생성자 추가해주는 어노테이션
+@AllArgsConstructor // 클래스 안 쪽의 모든 필드의 생성자 자동 추가 어노테이션
 @Data
-@Entity
-@Table(uniqueConstraints = {@UniqueConstraint(columnNames = "email")})
+@Entity // 엔티티 선언
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = "phoneNum")})
 public class UserEntity {
-    @Id
-    @GeneratedValue(generator = "system-uuid")
+    @Id  // entity의 대푯값 지정
+    @GeneratedValue(generator = "system-uuid")  //자동생성기능추가(숫자 자동으로 매겨짐)
     @GenericGenerator(name = "system-uuid", strategy = "uuid")
-    @Column(nullable=false)
-    @Column(nullable=false)
-    @Column(nullable=false)
+    private String id;
+    @Column(nullable=false) //username 필드 선언, db테이블의 username과 연결됨/ 값을 false로 설정해 주면, 해당 필드는 DDL 생성 시 not null이라는 조건이 붙은 채로 생성된다.
+    private String username;
+    @Column(nullable=false) // phoneNum 필드 선언 ,db 테이블의 phoneNum과 연결됨
+    private String phoneNum;
+    @Column(nullable=false) //password 필드 선언, db테이블의 password와 연결됨
+    private String password;
+    @Column(nullable = false)
+    private String birthday; // birthday 필드 선언, db테이블의 birthday와 연결
 
 }
